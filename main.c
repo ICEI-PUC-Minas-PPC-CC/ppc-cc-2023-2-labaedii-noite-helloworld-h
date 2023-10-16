60ad1a51143a3723c33c9af7a0d302a3f9232eef
Giovani Jacintho <Eliter760@hotmail.com>
2023-10-16 20:29:43 -0300

    main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "struct.c"
+#include "menu.h"
+#include <stdio.h>
+#include <locale.h>
+
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");//não ta funcionando, não sei o pq...
+    exibirMenu();
+
+}
+
+//Olá Diego! Tudo bem? Tivemos alguns problemas bobos, mas conseguimos fazer funcionar a primeira função funcionar certinho, portando
+//hoje durante a aula no Lab vamos implementar o resto das funções e provavelmente vamos atualizar os arquivos da programação, mas
+//por hora está aqui este aviso.
